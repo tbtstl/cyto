8b1ffd6e8b2f97f60255a89500a3e97e4a969ed1
tbtstl <tysonbattistella@gmail.com>
2023-06-28 14:37:07 -0400

    [feat] use pot20

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,13 +3,11 @@ set -e
 
 mkdir -p build
 
-if [ -f ./pot12_final.ptau ]; then
-    echo "pot12_final.ptau already exists. Skipping."
+if [ -f ./pot20_final.ptau ]; then
+    echo "pot20_final.ptau already exists. Skipping download."
 else
-    echo 'Creating pot12_final.ptau'
-    snarkjs powersoftau new bn128 12 pot12_0000.ptau -v;
-    snarkjs powersoftau contribute pot12_0000.ptau pot12_0001.ptau --name="First contribution" -v;
-    snarkjs powersoftau prepare phase2 pot12_0001.ptau pot12_final.ptau -v;
+    echo 'Downloading pot20...'
+    curl https://hermez.s3-eu-west-1.amazonaws.com/powersOfTau28_hez_final_20.ptau -o pot20_final.ptau
 fi
 
 echo "Compiling: main..."
@@ -23,13 +21,12 @@ duration=$(( SECONDS - start ))
 echo "Circuit compiled in $duration seconds"
 snarkjs r1cs info build/circom/main.r1cs
 
-# Start a new zkey and make a contribution
+# Create a new zkey
 echo "Creating zkey..."
 start=$SECONDS
-snarkjs plonk setup build/circom/main.r1cs pot12_final.ptau build/circom/circuit_final.zkey
+snarkjs plonk setup circuit.r1cs pot20_final.ptau circuit_final.zkey
 duration=$(( SECONDS - start ))
 echo "Zkey created in $duration seconds"
-# snarkjs zkey contribute build/circom/circuit_0000.zkey build/circom/circuit_final.zkey --name="1st Contributor Name" -v -e="$1"
 
 echo "Exporting verification key..."
 start=$SECONDS
