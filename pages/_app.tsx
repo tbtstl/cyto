228518ec5f32c7bc001478defaad806e1771ef55
tbtstl <tysonbattistella@gmail.com>
2024-01-23 00:00:38 -0500

    [feat] HEAD data

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,27 +7,40 @@ import { USE_MAINNET } from '../constants/utils'
 import { RainbowKitProvider, getDefaultWallets } from '@rainbow-me/rainbowkit'
 import { AppProps } from 'next/app'
 import { zoraSepolia } from "viem/chains";
+import Head from "next/head";
 
 const { chains, publicClient, webSocketPublicClient } = configureChains(
   [USE_MAINNET ? zora : zoraSepolia],
   [publicProvider()]
 );
 
-const { connectors } = getDefaultWallets({ appName: 'CYTO', projectId: process.env.NEXT_PUBLIC_WC_PROJECT_ID as string, chains })
+const { connectors } = getDefaultWallets({
+  appName: "CYTO",
+  projectId: process.env.NEXT_PUBLIC_WC_PROJECT_ID as string,
+  chains,
+});
 
 const config = createConfig({
-    autoConnect: true,
-    connectors,
-    publicClient,
-    webSocketPublicClient
-})
+  autoConnect: true,
+  connectors,
+  publicClient,
+  webSocketPublicClient,
+});
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-    return (
-        <WagmiConfig config={config}>
-            <RainbowKitProvider chains={chains}>
-                <Component {...pageProps} />
-            </RainbowKitProvider>
-        </WagmiConfig>
-    )
+  return (
+    <WagmiConfig config={config}>
+      <RainbowKitProvider chains={chains}>
+        <Head>
+          <title>CYTO</title>
+          <meta
+            name="description"
+            content="CYTO is a game of cellular evolution"
+          />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <Component {...pageProps} />
+      </RainbowKitProvider>
+    </WagmiConfig>
+  );
 }
